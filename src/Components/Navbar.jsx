b2c12a91a28fdbeb5d57f79c8c2ef630da81f39e
Naman <chachannaman@gmail.com>
2025-10-14 01:51:40 +0530

    some changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>
-      navbar
+    <div className='flex flex-row gap-4'>
+        <NavLink
+        to="/"
+        >
+            Home
+        </NavLink>
+        <NavLink
+        to="/pastes"
+        >
+           Pastes
+        </NavLink>
     </div>
   )
 }
