2ac09ac316c410d2e6eb162fa4a3f733f7ba677a
Naman <chachannaman@gmail.com>
2025-10-16 13:49:59 +0530

    home page changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>
-      home
-    </div>
-  )
+    const [title, setTitle] = useState('');
+    const [value , setValue] = useState('');
+    const [searchParams, setSearchParams] = useSearchParams();
+    const pasteID = searchParams.get("pasteID");
+
+    return (
+        <div>
+            <div className='flex flex-row gap-7'>
+            <input
+                className='p-2 rounded-2xl mt-2'
+                type="text"
+                placeholder='Enter the Title'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <button className='p-2 rounded-2xl mt-2'>
+                {   
+                    //conditional for creation and updation
+                    pasteID ? "Update Paste" 
+                    : "Create Paste"
+
+                }
+            </button>
+        </div>
+
+        <div>
+            <textarea
+                value={value}
+                placeholder='Enter your Content here ! '
+                onChange={(e) => setValue(e.target.value)}
+                rows={20}
+            />
+        </div>
+        </div>
+    )
 }
 
 export default Home
